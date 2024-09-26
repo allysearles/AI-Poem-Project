4ecf5729dd3935caf585c56206153df137ac9ff8
allysearles <alyssa.searles001@mymdc.net>
2024-09-26 18:00:24 -0400

    Worked on improving the user experience on Landing Page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayPoem(response) {
 	new Typewriter("#poem", {
 		strings: response.data.answer,
 		autoStart: true,
-		delay: 1,
+		delay: 2.0,
 		cursor: "",
 	});
 }
@@ -14,9 +14,13 @@ function generatePoem(event) {
 	let instructionsInput = document.querySelector("#user-instructions");
 	let apiKey = "bf3ac0311d9084536tao67043b4313ef";
 
-	let context = `You are a romantic French Poem expert and love to write short poems.Your mission is to generate a 4 line poem in basic HTML without replying back to me. Make sure to follow the user instructions below: and remove any extra ""'s or html phrases. Be sure to sign the poem at the very end with a <strong> SheCodes AI </strong>`;
+	let context = `You are a romantic French Poem expert and love to write short poems.Your mission is to generate a 4 line poem in basic HTML without replying back to me. Make sure to follow the user instructions below:. Be sure to sign the poem at the very end with a <strong> SheCodes AI </strong> and do not include any quotations or the word "html" in any of the text.`;
 	let prompt = `Generate a French Poem about ${instructionsInput.value}`;
 	let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+	let poemElement = document.querySelector("#poem");
+	poemElement.classList.remove("hidden");
+	poemElement.innerHTML = `<div class = "generating"> ⏳ Generating a poem about ${instructionsInput.value}. . .</div> `;
 
 	console.log("Generating Poem");
 	console.log(`Prompt: ${prompt}`);
